28f0bf8222808c1df775683c97be08e5854da617
ori mizrachi <orim553@gmail.com>
2015-10-19 15:22:46 +0300

    Initial Commit

---
+++ FirstGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstGame
+//
+//  Created by ori mizrachi on 10/19/15.
+//  Copyright © 2015 B-Up!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
